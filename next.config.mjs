274f49479b9446206969e341ddf00e7504f0a92c
khomma0312 <kijikuroneko@gmail.com>
2024-08-25 23:52:00 +0900

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.SUPABASE_DOMAIN,
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
+  // output: "export",
+};
 
 export default nextConfig;
